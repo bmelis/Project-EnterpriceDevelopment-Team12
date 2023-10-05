0387cc346120b997ea05105b802226a87d1e5f55
Bent Melis <r0831245@student.thomasmore.be>
2023-10-05 22:34:07 +0200

    added circuit api + container

--- circuit/src/main/java/fact/it/circuit/repository/CircuitRepository.java
+++ circuit/src/main/java/fact/it/circuit/repository/CircuitRepository.java
@@ -1,4 +1,11 @@
 package fact.it.circuit.repository;
 
-public interface CircuitRepository {
+import fact.it.circuit.model.Circuit;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CircuitRepository extends MongoRepository<Circuit, String> {
+    List<Circuit> findByName(List<String> name);
+
 }
