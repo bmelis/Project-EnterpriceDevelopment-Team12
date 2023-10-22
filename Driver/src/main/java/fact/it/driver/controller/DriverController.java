c1c3e8f8bb50162df8a419b1fda1a4444b26e4a8
Bent Melis <r0831245@student.thomasmore.be>
2023-10-22 14:37:39 +0200

    added api gateway

--- Driver/src/main/java/fact/it/driver/controller/DriverController.java
+++ Driver/src/main/java/fact/it/driver/controller/DriverController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/drivers")
+@RequestMapping("/api/driver")
 @RequiredArgsConstructor
 public class DriverController {
     private final DriverService driverService;
