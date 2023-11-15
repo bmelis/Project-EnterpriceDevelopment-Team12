2c4c102609adec06a6033f8cb329ad993b419d61
Bent Melis <r0831245@student.thomasmore.be>
2023-11-15 12:23:43 +0100

    try to fix error

--- race/src/main/java/fact/it/race/repository/RaceRepository.java
+++
@@ -1,7 +0,0 @@
-package fact.it.race.repository;
-
-import fact.it.race.model.Race;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RaceRepository extends JpaRepository<Race, Long> {
-}
