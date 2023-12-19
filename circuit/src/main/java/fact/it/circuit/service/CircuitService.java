b85435c4a89a416f6b60788b87190ed7a39400a6
Bent Melis <71631709+bmelis@users.noreply.github.com>
2023-12-19 12:16:58 +0100

    total api rework

--- circuit/src/main/java/fact/it/circuit/service/CircuitService.java
+++ circuit/src/main/java/fact/it/circuit/service/CircuitService.java
@@ -5,33 +5,31 @@ import fact.it.circuit.dto.CircuitResponse;
 import fact.it.circuit.model.Circuit;
 import fact.it.circuit.repository.CircuitRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CircuitService {
     private final CircuitRepository circuitRepository;
-
-    public void createCircuit(CircuitRequest circuitRequest){
-        Circuit circuit = Circuit.builder()
-                .name(circuitRequest.getName())
-                .lenght(circuitRequest.getLenght())
-                .country(circuitRequest.getCountry())
-                .build();
-
-        circuitRepository.save(circuit);
-    }
     public List<CircuitResponse> getAllCircuits() {
         List<Circuit> products = circuitRepository.findAll();
 
         return products.stream().map(this::mapToCircuitResponse).toList();
     }
-    public List<CircuitResponse> getAllCircuitsByName(List<String> name) {
-        List<Circuit> products = circuitRepository.findByName(name);
-
-        return products.stream().map(this::mapToCircuitResponse).toList();
+    public ResponseEntity<CircuitResponse> getCircuitById(int id) {
+        Optional<Circuit> optionalCircuit = circuitRepository.findById(id);
+        if(optionalCircuit.isPresent()) {
+            Circuit circuit = optionalCircuit.get();
+            return ResponseEntity.ok(mapToCircuitResponse(circuit));
+        } else {
+            // Return a ResponseEntity with a 404 Not Found status
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     private CircuitResponse mapToCircuitResponse(Circuit circuit) {
         return CircuitResponse.builder()
