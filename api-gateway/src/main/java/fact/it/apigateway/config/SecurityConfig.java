2657140cf721a610bdc7d6e583dcdad805509b26
Bent Melis <71631709+bmelis@users.noreply.github.com>
2023-12-20 13:39:35 +0100

    hopefully fix api-gateway error

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET,"/circuit","/circuit/{id}", "/team", "/team/{id}","/driver/{id}","/race", "/race/{id}")
+                        exchange.pathMatchers(HttpMethod.GET,"/circuit","/circuit/{id}", "/team", "/team/{id}","/driver/{teamId}","/race", "/race/{id}")
                                 .permitAll()
                                 .anyExchange()
                                 .authenticated()
